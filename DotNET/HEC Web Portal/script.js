8b83badb89dc4ec61b919fbc9471652cec2e1d54
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-01-02 23:05:29 +0500

    Resolved iframe height issue by 70% 02-01-2023

--- DotNET/HEC Web Portal/script.js
+++ DotNET/HEC Web Portal/script.js
@@ -1,30 +1,33 @@
-$(document).ready(function(){
-   document.getElementById("nav-contact").setAttribute('hidden', true)
-   document.getElementById("nav-edu").setAttribute('hidden', true)
-   document.getElementById("nav-emp").setAttribute('hidden', true)
-});
+// $(document).ready(function(){
+// });
 
-$('#nav-personal-tab').on('click', function(event) {        
-   document.getElementById("nav-personal").setAttribute('hidden', false)
-});
+// $('#nav-personal-tab').on('click', function(event) {        
+//    document.getElementById("nav-personal").setAttribute('hidden', false)
+// });
 
-$('#nav-contact-tab').on('click', function(event) {        
-   document.getElementById("nav-contact").setAttribute('hidden', false)
-});
+// $('#nav-contact-tab').on('click', function(event) {        
+//    document.getElementById("nav-contact").setAttribute('hidden', false)
+// });
 
-$('#nav-edu-tab').on('click', function(event) {        
-   document.getElementById("nav-edu").setAttribute('hidden', false)
-});
+// $('#nav-edu-tab').on('click', function(event) {        
+//    document.getElementById("nav-edu").setAttribute('hidden', false)
+// });
 
-$('#nav-emp-tab').on('click', function(event) {        
-   document.getElementById("nav-emp").setAttribute('hidden', false)
-});
-/*
-$(".tab").click(function () {
-    $(".tab").removeClass("active");
-    // $(".tab").addClass("active"); // instead of this do the below 
-    $(this).addClass("active");   
-});
+// $('#nav-emp-tab').on('click', function(event) {        
+//    document.getElementById("nav-emp").setAttribute('hidden', false)
+// });
 
-$('#about').setAttribute('hidden', true)
-*/
+// $(".tab").click(function () {
+//     $(".tab").removeClass("active");
+//     // $(".tab").addClass("active"); // instead of this do the below 
+//     $(this).addClass("active");   
+// });
+
+// $('#about').setAttribute('hidden', true)
+//Set iFrame Height
+function resizeIframe() {
+
+   //obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+   $('iframe').height($('iframe').contents().height());
+
+ }
