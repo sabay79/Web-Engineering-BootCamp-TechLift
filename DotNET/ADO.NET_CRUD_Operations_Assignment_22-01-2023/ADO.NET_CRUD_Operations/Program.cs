34b9016ea8081ce5ae965d32da408d2ae73c5b11
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-01-22 18:33:54 +0500

    ASP.NET CRUD

--- DotNET/ADO.NET_CRUD_Operations_Assignment_22-01-2023/ADO.NET_CRUD_Operations/Program.cs
+++ DotNET/ADO.NET_CRUD_Operations_Assignment_22-01-2023/ADO.NET_CRUD_Operations/Program.cs
@@ -18,15 +18,44 @@ namespace ADO.NET_CRUD_Operations
             connection.Open();
 
             // Prepare reader to loop through the table
-            using SqlDataReader reader = sqlCommand.ExecuteReader();
-
-            // Getting each row one by one
-            while (reader.Read())
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
             {
-                // Print User Name here 
-                Console.WriteLine(reader.GetString(1));
+                // Getting each row one by one
+                while (reader.Read())
+                {
+                    // Print User Name here 
+                    Console.WriteLine(reader.GetString(1));
+                }
             }
-            Console.WriteLine("THE END");
+
+            // INSERT Operation
+            string insertUserQuery = "INSERT INTO [User] (ID,Name) VALUES (@ID, @Name)";
+            SqlCommand insertCommand = new SqlCommand(insertUserQuery, connection);
+            insertCommand.Parameters.AddWithValue("@ID", 3);
+            insertCommand.Parameters.AddWithValue("@Name", "Yamna");
+
+            int recordsAdded = insertCommand.ExecuteNonQuery();
+            Console.WriteLine($"{recordsAdded} no of records added");
+
+            // UPDATE Operation
+            string updateUserQuery = "UPDATE [User] SET Name=@Name WHERE ID=@ID";
+            SqlCommand updateCommand = new SqlCommand(updateUserQuery, connection);
+            updateCommand.Parameters.AddWithValue("@Name", "SabaY");
+            updateCommand.Parameters.AddWithValue("@ID", 1);
+
+            int recordsUpdated = updateCommand.ExecuteNonQuery();
+            Console.WriteLine($"{recordsUpdated} no of records updated");
+
+            // DELETE Operations
+            string deleteUserQuery = "DELETE * FROM [User] WHERE ID=@ID";
+            SqlCommand deleteCommand = new SqlCommand(deleteUserQuery, connection);
+            deleteCommand.Parameters.AddWithValue("@ID", 1);
+
+            int recordsDeleted = updateCommand.ExecuteNonQuery();
+            Console.WriteLine($"{recordsDeleted} no of records deleted");
+
+
+            Console.WriteLine("\n\nTHE END");
         }
     }
 }
