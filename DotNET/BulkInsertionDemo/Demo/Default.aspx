7ec6d1c33196bcb3f170eb80c45f2051149cd7ae
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-01-28 16:35:12 +0500

    Bulk Insertion Demo

---
+++ DotNET/BulkInsertionDemo/Demo/Default.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Demo._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    
+    <form id="form1">
+        <h6 style="color:white; font-size:medium; font-weight: bolder;">Hello</h6>
+
+        <table class="table" style="color:black;margin-top:25px" >
+            <tr style="background-color:#f0ad4e; color:white; font-weight:bold">
+                <td>Product Name</td>
+                <td>Brand Name</td>
+                <td>Warrenty</td>
+                <td>Price</td>
+            </tr>
+            <tr style="color:black">
+                <td> 
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox> 
+                </td>
+                <td> 
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr style="color:black">
+                <td></td>
+                <td></td> 
+                <td></td>
+                <td style="float:right"> 
+                    <asp:Button  class="btn btn-warning" ID="AddProduct" runat="server" Text="Add Product" 
+                        onclick="AddProduct_Click"/>
+                </td>
+            </tr>
+        </table>  
+        
+        <div style="margin-top:20px">
+            <asp:GridView class="table" ID="GridView1" AutoGenerateColumns="false" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <Columns>
+                    <asp:BoundField  HeaderStyle-Width="120px" HeaderText="Product Name" DataField="ProductName"/>
+                    <asp:BoundField HeaderStyle-Width="120px" HeaderText="Brand Name" DataField="BrandName"/>
+                    <asp:BoundField HeaderStyle-Width="120px" HeaderText="Warrenty" DataField="Warrenty"/>
+                    <asp:BoundField  HeaderStyle-Width="120px" HeaderText="Price" DataField="Price"/>
+                </Columns>
+                <EditRowStyle BackColor="#2461BF" />
+                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#5cb85c" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EFF3FB" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+            </asp:GridView>
+        </div>
+        <div style="margin-top:10px; float:right">
+            <asp:Button class="btn btn-success" ID="btnsubmitProducts" runat="server"
+                Text="Save Products" onclick="btnsubmitProducts_Click" />
+        </div>
+    </form>
+</asp:Content>
---
+++ DotNET/BulkInsertionDemo/Demo/Default.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+namespace Demo
+{
+    public partial class _Default : Page
+    {
+        public string constr;
+        public SqlConnection con;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                AddDefaultFirstRecord();
+            }
+        }
+
+        public void connection()
+        {
+            //Stoting connection string 
+            constr = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+            con = new SqlConnection(constr);
+            con.Open();
+
+        }
+        protected void AddProduct_Click(object sender, EventArgs e)
+        {
+            AddNewRecordRowToGrid();
+        }
+
+        private void AddDefaultFirstRecord()
+        {
+            //creating DataTable
+            DataTable dt = new DataTable();
+            DataRow dr;
+            dt.TableName = "ProductsSold";
+            //creating columns for DataTable
+            dt.Columns.Add(new DataColumn("ProductName", typeof(string)));
+            dt.Columns.Add(new DataColumn("BrandName", typeof(string)));
+            dt.Columns.Add(new DataColumn("Warrenty", typeof(int)));
+            dt.Columns.Add(new DataColumn("Price", typeof(double)));
+            dr = dt.NewRow();
+            dt.Rows.Add(dr);
+
+            ViewState["ProductsSold"] = dt;
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+        private void AddNewRecordRowToGrid()
+        {
+            if (ViewState["ProductsSold"] != null)
+            {
+                DataTable dtCurrentTable = (DataTable)ViewState["ProductsSold"];
+                DataRow drCurrentRow = null;
+
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+                        //Creating new row and assigning values
+                        drCurrentRow = dtCurrentTable.NewRow();
+                        drCurrentRow["ProductName"] = TextBox1.Text;
+                        drCurrentRow["BrandName"] = TextBox2.Text;
+                        drCurrentRow["Warrenty"] = Convert.ToInt32(TextBox3.Text);
+                        drCurrentRow["Price"] = Convert.ToDouble(TextBox4.Text);
+                    }
+                    //Removing initial blank row
+                    if (dtCurrentTable.Rows[0][0].ToString() == "")
+                    {
+                        dtCurrentTable.Rows[0].Delete();
+                        dtCurrentTable.AcceptChanges();
+                    }
+
+                    //Added New Record to the DataTable
+                    dtCurrentTable.Rows.Add(drCurrentRow);
+                    //storing DataTable to ViewState
+                    ViewState["ProductsSold"] = dtCurrentTable;
+                    //binding Gridview with New Row
+                    GridView1.DataSource = dtCurrentTable;
+                    GridView1.DataBind();
+                }
+            }
+        }
+
+        protected void btnsubmitProducts_Click(object sender, EventArgs e)
+        {
+            BulkInsertToDataBase();
+        }
+
+        private void BulkInsertToDataBase()
+        {
+            DataTable dtProductSold = (DataTable)ViewState["ProductsSold"];
+            connection();
+            //creating object of SqlBulkCopy
+            SqlBulkCopy objbulk = new SqlBulkCopy(con);
+            //assigning Destination table name
+            objbulk.DestinationTableName = "Product";
+            //Mapping Table column
+            objbulk.ColumnMappings.Add("ProductName", "ProductName");
+            objbulk.ColumnMappings.Add("BrandName", "BrandName");
+            objbulk.ColumnMappings.Add("Warrenty", "Warrenty");
+            objbulk.ColumnMappings.Add("Price", "Price");
+            //inserting bulk Records into DataBase 
+            objbulk.WriteToServer(dtProductSold);
+        }
+    }
+}
