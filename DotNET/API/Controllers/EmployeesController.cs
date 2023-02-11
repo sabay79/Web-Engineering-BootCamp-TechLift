c4b318989a492fab74ab58132401c875b0d6e414
Saba Yashfeen <sabamukhtar79@gmail.com>
2023-02-11 21:20:53 +0500

    Web API Project Created

---
+++ DotNET/API/Controllers/EmployeesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Api;
+
+namespace Api.Controllers
+{
+    [RoutePrefix("api/Emp")]
+    public class EmployeesController : ApiController
+    {
+        private DBCompanyEntities db = new DBCompanyEntities();
+
+        // GET: api/Employees
+        [HttpGet]
+        [Route("AllEmployee")]
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+        // GET: api/Employees/5
+        [HttpGet]
+        [ResponseType(typeof(Employee))]
+        [Route("SpecificEmployee")]
+        public async Task<IHttpActionResult> GetEmployee(int id,string state)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        // PUT: api/Employees/5
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        [Route("UpdateEmployee")]
+        public async Task<IHttpActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employee.EMPLOYEE_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Employees
+        [HttpPost]
+        [ResponseType(typeof(Employee))]
+        [Route("AddEmployee")]
+        public async Task<IHttpActionResult> PostEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Employees.Add(employee);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = employee.EMPLOYEE_ID }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete]
+        [ResponseType(typeof(Employee))]
+        [Route("DeleteEmployee")]
+        public async Task<IHttpActionResult> DeleteEmployee(int id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return db.Employees.Count(e => e.EMPLOYEE_ID == id) > 0;
+        }
+    }
+}
---
+++ DotNET/API/DbCompany.edmx.sql
@@ -0,0 +1,95 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 02/11/2023 21:14:33
+-- Generated from EDMX file: C:\Users\Saba\source\repos\sabay79\Web-Engineering-BootCamp-TechLift\DotNET\API\DbCompany.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [DBCompany];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Employee]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Employee];
+GO
+IF OBJECT_ID(N'[dbo].[tblEmployee]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[tblEmployee];
+GO
+IF OBJECT_ID(N'[DBCompanyModelStoreContainer].[Incentives]', 'U') IS NOT NULL
+    DROP TABLE [DBCompanyModelStoreContainer].[Incentives];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Employees'
+CREATE TABLE [dbo].[Employees] (
+    [EMPLOYEE_ID] int IDENTITY(1,1) NOT NULL,
+    [FIRST_NAME] nvarchar(50)  NULL,
+    [LAST_NAME] nvarchar(50)  NULL,
+    [SALARY] int  NULL,
+    [JOINING_DATE] nvarchar(100)  NULL,
+    [DEPARTMENT] nvarchar(20)  NULL
+);
+GO
+
+-- Creating table 'tblEmployees'
+CREATE TABLE [dbo].[tblEmployees] (
+    [EmployeeId] int  NOT NULL,
+    [Name] nvarchar(20)  NULL,
+    [ManagerId] int  NULL
+);
+GO
+
+-- Creating table 'Incentives'
+CREATE TABLE [dbo].[Incentives] (
+    [EMPLOYEE_REF_ID] nchar(10)  NOT NULL,
+    [INCENTIVE_DATE] nvarchar(50)  NULL,
+    [INCENTIVE_AMOUNT] int  NULL,
+    [Total] int  NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [EMPLOYEE_ID] in table 'Employees'
+ALTER TABLE [dbo].[Employees]
+ADD CONSTRAINT [PK_Employees]
+    PRIMARY KEY CLUSTERED ([EMPLOYEE_ID] ASC);
+GO
+
+-- Creating primary key on [EmployeeId] in table 'tblEmployees'
+ALTER TABLE [dbo].[tblEmployees]
+ADD CONSTRAINT [PK_tblEmployees]
+    PRIMARY KEY CLUSTERED ([EmployeeId] ASC);
+GO
+
+-- Creating primary key on [EMPLOYEE_REF_ID] in table 'Incentives'
+ALTER TABLE [dbo].[Incentives]
+ADD CONSTRAINT [PK_Incentives]
+    PRIMARY KEY CLUSTERED ([EMPLOYEE_REF_ID] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
